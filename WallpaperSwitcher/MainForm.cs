c91b94110b3c1d8a559003ed634186b2d9bb1392
Nate Shoffner <nate.shoffner@gmail.com>
2023-05-27 04:02:05 -0400

    Missed in previoius commit

--- WallpaperSwitcher/MainForm.cs
+++ WallpaperSwitcher/MainForm.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using System.Management;
+using WallpaperSwitcher.Forms;
 using static WallpaperSwitcher.ManagedScreen;
 
 namespace WallpaperSwitcher
@@ -29,40 +30,16 @@ namespace WallpaperSwitcher
 
             Screen[] screens = Screen.AllScreens;
 
-            // loop through all the screens with a starting index of 1
-
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_DesktopMonitor");
-
-            foreach (ManagementObject queryObj in searcher.Get())
-            {
-                Debug.WriteLine("-----------------------------------");
-                Debug.WriteLine("Win32_DesktopMonitor instance");
-                Debug.WriteLine("-----------------------------------");
-                Debug.WriteLine("Description: {0}", queryObj["Description"]);
-                Debug.WriteLine("DeviceID: {0}", queryObj["DeviceID"]);
-                Debug.WriteLine("MonitorManufacturer: {0}", queryObj["MonitorManufacturer"]);
-                Debug.WriteLine("MonitorType: {0}", queryObj["MonitorType"]);
-                Debug.WriteLine("Name: {0}", queryObj["Name"]);
-                Debug.WriteLine("PixelsPerXLogicalInch: {0}", queryObj["PixelsPerXLogicalInch"]);
-                Debug.WriteLine("PixelsPerYLogicalInch: {0}", queryObj["PixelsPerYLogicalInch"]);   
-                Debug.WriteLine("ScreenHeight: {0}", queryObj["ScreenHeight"]);
-
-            }
-
             for (int i = 0; i < screens.Length; i++)
             {
                 var id = i + 1;
-                Screen screen = screens[i];
-                string deviceName = screen.DeviceName;
 
-                ManagedScreen managedScreen = new ManagedScreen(screen, id);
+                ManagedScreen managedScreen = new ManagedScreen(screens[i], id);
                 managedScreen.LoadSettings();
                 managedScreens.Add(managedScreen);
-            
 
-            ScreenControl screenControl = new ScreenControl(managedScreen, deviceName);
-
-                flowLayoutPanel1.Controls.Add(screenControl);
+                var control = CreateScreenControl(managedScreen);
+                flowLayoutPanel1.Controls.Add(control);
             }
 
             lblScreens.Text = $"Screens: {screens.Length}";
@@ -75,27 +52,30 @@ namespace WallpaperSwitcher
 
         private async void identifyScreensToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var forms = new List<IdentityForm>();
-
-            foreach (ManagedScreen screen in managedScreens)
+            foreach (var screen in managedScreens)
             {
-                IdentityForm identityForm = new IdentityForm(screen.Screen, screen.Id.ToString());
-                forms.Add(identityForm);
-                identityForm.Show();
+                ShowIdentification(screen);
             }
+        }
 
-            await Task.Delay(2000);
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
-            foreach (IdentityForm form in forms)
+        private ScreenControl CreateScreenControl(ManagedScreen screen)
+        {
+            ScreenControl screenControl = new ScreenControl(screen);
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add(new ToolStripMenuItem() { Text = "Identify" });
+            screenControl.ContextMenuStrip = contextMenu;
+            return screenControl;
+        }
+
         private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             using (var dialog = new SettingsForm())
             {
-                form.Close();
                 if (dialog.ShowDialog() == DialogResult.OK)
                 {
 
@@ -103,9 +83,10 @@ namespace WallpaperSwitcher
             }
         }
 
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        private void ShowIdentification(ManagedScreen screen)
         {
-            Application.Exit();
+            IdentityForm identityForm = new IdentityForm(screen, 2000);
+            identityForm.Show();
         }
     }
 }
