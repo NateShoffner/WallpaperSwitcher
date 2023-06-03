3f9730bc53de0c5b37205085d9792d2c99657e15
Nate Shoffner <nate.shoffner@gmail.com>
2023-06-03 10:45:56 -0400

    Dark theming w/ drawer icons

--- WallpaperSwitcher/Forms/MainForm.Designer.cs
+++ WallpaperSwitcher/Forms/MainForm.Designer.cs
@@ -28,18 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             screenLayoutPanel1 = new Controls.ScreenLayoutPanel();
             materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
             tabPage1 = new TabPage();
             panel1 = new Panel();
+            materialButton3 = new MaterialSkin.Controls.MaterialButton();
+            materialButton2 = new MaterialSkin.Controls.MaterialButton();
+            materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            btnRefresh = new MaterialSkin.Controls.MaterialButton();
             tabPage2 = new TabPage();
             materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
             tabPage3 = new TabPage();
             materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
-            btnRefresh = new MaterialSkin.Controls.MaterialButton();
-            materialButton1 = new MaterialSkin.Controls.MaterialButton();
-            materialButton2 = new MaterialSkin.Controls.MaterialButton();
-            materialButton3 = new MaterialSkin.Controls.MaterialButton();
+            drawerImageList = new ImageList(components);
             materialTabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             panel1.SuspendLayout();
@@ -49,6 +51,7 @@
             // 
             // screenLayoutPanel1
             // 
+            screenLayoutPanel1.AutoSize = true;
             screenLayoutPanel1.BackColor = Color.LightSkyBlue;
             screenLayoutPanel1.Dock = DockStyle.Fill;
             screenLayoutPanel1.IsResizing = false;
@@ -57,7 +60,7 @@
             screenLayoutPanel1.ShowScreenModels = true;
             screenLayoutPanel1.ShowScreenNumbers = true;
             screenLayoutPanel1.ShowWallpaperPreviews = true;
-            screenLayoutPanel1.Size = new Size(946, 353);
+            screenLayoutPanel1.Size = new Size(946, 338);
             screenLayoutPanel1.TabIndex = 6;
             // 
             // materialTabControl1
@@ -67,6 +70,7 @@
             materialTabControl1.Controls.Add(tabPage3);
             materialTabControl1.Depth = 0;
             materialTabControl1.Dock = DockStyle.Fill;
+            materialTabControl1.ImageList = drawerImageList;
             materialTabControl1.Location = new Point(3, 64);
             materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
             materialTabControl1.Multiline = true;
@@ -80,10 +84,10 @@
             tabPage1.AutoScroll = true;
             tabPage1.Controls.Add(screenLayoutPanel1);
             tabPage1.Controls.Add(panel1);
-            tabPage1.Location = new Point(4, 24);
+            tabPage1.Location = new Point(4, 39);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(952, 405);
+            tabPage1.Size = new Size(952, 390);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Displays";
             tabPage1.UseVisualStyleBackColor = true;
@@ -95,119 +99,10 @@
             panel1.Controls.Add(materialButton1);
             panel1.Controls.Add(btnRefresh);
             panel1.Dock = DockStyle.Bottom;
-            panel1.Location = new Point(3, 356);
+            panel1.Location = new Point(3, 341);
             panel1.Name = "panel1";
             panel1.Size = new Size(946, 46);
             panel1.TabIndex = 9;
-            // 
-            // tabPage2
-            // 
-            tabPage2.Controls.Add(materialLabel2);
-            tabPage2.Location = new Point(4, 24);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(952, 405);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "Settings";
-            tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // materialLabel2
-            // 
-            materialLabel2.AutoSize = true;
-            materialLabel2.Depth = 0;
-            materialLabel2.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
-            materialLabel2.Location = new Point(6, 3);
-            materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
-            materialLabel2.Name = "materialLabel2";
-            materialLabel2.Size = new Size(59, 19);
-            materialLabel2.TabIndex = 9;
-            materialLabel2.Text = "Settings";
-            // 
-            // tabPage3
-            // 
-            tabPage3.Controls.Add(materialLabel3);
-            tabPage3.Location = new Point(4, 24);
-            tabPage3.Name = "tabPage3";
-            tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(952, 405);
-            tabPage3.TabIndex = 2;
-            tabPage3.Text = "About...";
-            tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // materialLabel3
-            // 
-            materialLabel3.AutoSize = true;
-            materialLabel3.Depth = 0;
-            materialLabel3.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
-            materialLabel3.Location = new Point(6, 3);
-            materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
-            materialLabel3.Name = "materialLabel3";
-            materialLabel3.Size = new Size(43, 19);
-            materialLabel3.TabIndex = 9;
-            materialLabel3.Text = "About";
-            // 
-            // btnRefresh
-            // 
-            btnRefresh.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            btnRefresh.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            btnRefresh.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
-            btnRefresh.Depth = 0;
-            btnRefresh.HighEmphasis = true;
-            btnRefresh.Icon = null;
-            btnRefresh.Location = new Point(4, 4);
-            btnRefresh.Margin = new Padding(4, 6, 4, 6);
-            btnRefresh.MouseState = MaterialSkin.MouseState.HOVER;
-            btnRefresh.Name = "btnRefresh";
-            btnRefresh.NoAccentTextColor = Color.Empty;
-            btnRefresh.Size = new Size(148, 36);
-            btnRefresh.TabIndex = 8;
-            btnRefresh.Text = "Reload Displays";
-            btnRefresh.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            btnRefresh.UseAccentColor = false;
-            btnRefresh.UseVisualStyleBackColor = true;
-            btnRefresh.Click += LoadScreens;
-            // 
-            // materialButton1
-            // 
-            materialButton1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            materialButton1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
-            materialButton1.Depth = 0;
-            materialButton1.HighEmphasis = true;
-            materialButton1.Icon = null;
-            materialButton1.Location = new Point(160, 4);
-            materialButton1.Margin = new Padding(4, 6, 4, 6);
-            materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
-            materialButton1.Name = "materialButton1";
-            materialButton1.NoAccentTextColor = Color.Empty;
-            materialButton1.Size = new Size(156, 36);
-            materialButton1.TabIndex = 9;
-            materialButton1.Text = "Identify Displays";
-            materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            materialButton1.UseAccentColor = false;
-            materialButton1.UseVisualStyleBackColor = true;
-            materialButton1.Click += IdentifyScreens;
-            // 
-            // materialButton2
-            // 
-            materialButton2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            materialButton2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            materialButton2.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
-            materialButton2.Depth = 0;
-            materialButton2.HighEmphasis = true;
-            materialButton2.Icon = null;
-            materialButton2.Location = new Point(637, 4);
-            materialButton2.Margin = new Padding(4, 6, 4, 6);
-            materialButton2.MouseState = MaterialSkin.MouseState.HOVER;
-            materialButton2.Name = "materialButton2";
-            materialButton2.NoAccentTextColor = Color.Empty;
-            materialButton2.Size = new Size(132, 36);
-            materialButton2.TabIndex = 10;
-            materialButton2.Text = "Show Desktop";
-            materialButton2.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            materialButton2.UseAccentColor = false;
-            materialButton2.UseVisualStyleBackColor = true;
-            materialButton2.Click += ShowDesktop;
             // 
             // materialButton3
             // 
@@ -230,12 +125,128 @@
             materialButton3.UseVisualStyleBackColor = true;
             materialButton3.Click += OpenDisplaySettings;
             // 
+            // materialButton2
+            // 
+            materialButton2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            materialButton2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialButton2.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            materialButton2.Depth = 0;
+            materialButton2.HighEmphasis = true;
+            materialButton2.Icon = null;
+            materialButton2.Location = new Point(637, 4);
+            materialButton2.Margin = new Padding(4, 6, 4, 6);
+            materialButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            materialButton2.Name = "materialButton2";
+            materialButton2.NoAccentTextColor = Color.Empty;
+            materialButton2.Size = new Size(132, 36);
+            materialButton2.TabIndex = 10;
+            materialButton2.Text = "Show Desktop";
+            materialButton2.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            materialButton2.UseAccentColor = false;
+            materialButton2.UseVisualStyleBackColor = true;
+            materialButton2.Click += ShowDesktop;
+            // 
+            // materialButton1
+            // 
+            materialButton1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            materialButton1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            materialButton1.Depth = 0;
+            materialButton1.HighEmphasis = true;
+            materialButton1.Icon = null;
+            materialButton1.Location = new Point(160, 4);
+            materialButton1.Margin = new Padding(4, 6, 4, 6);
+            materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialButton1.Name = "materialButton1";
+            materialButton1.NoAccentTextColor = Color.Empty;
+            materialButton1.Size = new Size(156, 36);
+            materialButton1.TabIndex = 9;
+            materialButton1.Text = "Identify Displays";
+            materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            materialButton1.UseAccentColor = false;
+            materialButton1.UseVisualStyleBackColor = true;
+            materialButton1.Click += IdentifyScreens;
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnRefresh.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnRefresh.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            btnRefresh.Depth = 0;
+            btnRefresh.HighEmphasis = true;
+            btnRefresh.Icon = null;
+            btnRefresh.Location = new Point(4, 4);
+            btnRefresh.Margin = new Padding(4, 6, 4, 6);
+            btnRefresh.MouseState = MaterialSkin.MouseState.HOVER;
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.NoAccentTextColor = Color.Empty;
+            btnRefresh.Size = new Size(148, 36);
+            btnRefresh.TabIndex = 8;
+            btnRefresh.Text = "Reload Displays";
+            btnRefresh.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            btnRefresh.UseAccentColor = false;
+            btnRefresh.UseVisualStyleBackColor = true;
+            btnRefresh.Click += LoadScreens;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(materialLabel2);
+            tabPage2.Location = new Point(4, 39);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(952, 390);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Settings";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // materialLabel2
+            // 
+            materialLabel2.AutoSize = true;
+            materialLabel2.Depth = 0;
+            materialLabel2.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialLabel2.Location = new Point(6, 3);
+            materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel2.Name = "materialLabel2";
+            materialLabel2.Size = new Size(59, 19);
+            materialLabel2.TabIndex = 9;
+            materialLabel2.Text = "Settings";
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(materialLabel3);
+            tabPage3.Location = new Point(4, 39);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(952, 390);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "About...";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // materialLabel3
+            // 
+            materialLabel3.AutoSize = true;
+            materialLabel3.Depth = 0;
+            materialLabel3.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialLabel3.Location = new Point(6, 3);
+            materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel3.Name = "materialLabel3";
+            materialLabel3.Size = new Size(43, 19);
+            materialLabel3.TabIndex = 9;
+            materialLabel3.Text = "About";
+            // 
+            // drawerImageList
+            // 
+            drawerImageList.ColorDepth = ColorDepth.Depth32Bit;
+            drawerImageList.ImageSize = new Size(32, 32);
+            drawerImageList.TransparentColor = Color.Transparent;
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(966, 500);
             Controls.Add(materialTabControl1);
+            DrawerShowIconsWhenHidden = true;
             DrawerTabControl = materialTabControl1;
             DrawerUseColors = true;
             MinimumSize = new Size(860, 500);
@@ -245,6 +256,7 @@
             SizeChanged += MainForm_SizeChanged;
             materialTabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             tabPage2.ResumeLayout(false);
@@ -268,5 +280,6 @@
         private MaterialSkin.Controls.MaterialButton materialButton1;
         private MaterialSkin.Controls.MaterialButton materialButton2;
         private MaterialSkin.Controls.MaterialButton materialButton3;
+        private ImageList drawerImageList;
     }
 }
--- WallpaperSwitcher/Forms/MainForm.cs
+++ WallpaperSwitcher/Forms/MainForm.cs
@@ -1,4 +1,5 @@
 
+using FontAwesome.Sharp;
 using MaterialSkin;
 using MaterialSkin.Controls;
 using Shell32;
@@ -16,16 +17,27 @@ namespace WallpaperSwitcher
 
             _previousWindowState = WindowState;
 
+            // use tab header as key since index doesn't work for some reason, might be unique to the material drawer implementation?
+            drawerImageList.Images.Add(tabPage1.Text, IconChar.Display.ToBitmap(Color.White));
+            drawerImageList.Images.Add(tabPage2.Text, IconChar.Gear.ToBitmap(Color.White));
+            drawerImageList.Images.Add(tabPage3.Text, IconChar.Info.ToBitmap(Color.White));
+
+            foreach (TabPage tabPage in materialTabControl1.TabPages)
+            {
+                tabPage.ImageKey = tabPage.Text;
+            }
+
             var materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
-            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+
             //materialSkinManager.ColorScheme = new ColorScheme(Primary.Red200, Primary.Purple100, Primary.Teal100, Accent.Blue200, TextShade.BLACK);
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
 
             screenLayoutPanel1.ScreenIdentifyClick += ShowIdentification;
 
             LoadScreens();
         }
-
 
         private void LoadScreens(object sender = null, EventArgs e = null)
         {
